6bcdc8480e54f6cc66244fadd9103c77ce30ad22
AndrewKim <andrew.kim@aionys.com>
2021-07-28 10:47:04 +0300

    Added base models, added base repository

---
+++ TheShop.Repositories/BaseRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using TheShop.Models.Base;
+using TheShop.Repositories.Context;
+using TheShop.Repositories.Interfaces;
+
+namespace TheShop.Repositories
+{
+	public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : BaseModel
+	{
+		public BaseRepository(IDBContext context)
+		{
+			DBContext = context;
+		}
+
+		public IDBContext DBContext { get; set; }
+
+		public virtual int Create(TEntity entity)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public virtual IEnumerable<TEntity> GetAll()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public virtual TEntity GetById(int id)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public virtual void Remove(int id)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public virtual bool Update(TEntity entity)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
---
+++ TheShop.Repositories/Context/IDBContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using TheShop.Models.Entities;
+
+namespace TheShop.Repositories.Context
+{
+	public interface IDBContext
+	{
+		List<Article> Articles { get; set; }
+
+		bool SaveChanges();
+	}
+}
